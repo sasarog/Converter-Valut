1abf5627aa75b7eaeae96bd156f2e985825f5f50
Aleksandr Rogalevich <sasa13481@gmail.com>
2021-03-29 18:14:27 +0300

    Сделал получение курса по Id.

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -18,11 +18,49 @@ using System.Xml;
 
 namespace WpfApp1
 {
+
+   
+
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window 
     {
+        Rate GetRateById(int id)
+        {
+            String url = "https://www.nbrb.by/api/exrates/Rates/" + Convert.ToString(id);
+            //объект, который будет соединяться с сайтом
+            System.Net.WebClient client = new System.Net.WebClient();
+            //объект для десериализации строки (дешифратор)
+            JavaScriptSerializer serializer = new JavaScriptSerializer();
+            //Строка в формате json
+            //{"Cur_ID":298,"Date":"2021-03-18T00:00:00","Cur_Abbreviation":"RUB"
+            String json = client.DownloadString(url);
+            //преобразование строки в объект класса Rate
+            Rate rate = serializer.Deserialize<Rate>(json);
+            MessageBox.Show(rate.Cur_Abbreviation.ToString());
+            return rate;
+        }
+        void GetRateByName(string name)
+        {
+            String url = "https://www.nbrb.by/api/exrates/currencies";
+            //объект, который будет соединяться с сайтом
+            System.Net.WebClient client = new System.Net.WebClient();
+            //объект для десериализации строки (дешифратор)
+            JavaScriptSerializer serializer = new JavaScriptSerializer();
+            //Строка в формате json
+            //{"Cur_ID":298,"Date":"2021-03-18T00:00:00","Cur_Abbreviation":"RUB
+            String json = client.DownloadString(url);
+            //преобразование строки в объект класса Rate
+            //Rate izChego = serializer.Deserialize<Rate>(json);
+            List <Currency> currency = new List<Currency>();
+            //Currency c = serializer.Deserialize<Currency>(json);
+
+            // currency.Add( serializer.Deserialize<Currency>(json));
+            MessageBox.Show(currency.Capacity.ToString());
+            //return currency.First<Currency>;
+        }
+
         public MainWindow()
         {
             InitializeComponent();
@@ -34,120 +72,12 @@ namespace WpfApp1
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             //строка для запроса
-            String url = "https://www.nbrb.by/api/exrates/rates/";
-            String url2 = "https://www.nbrb.by/api/exrates/rates/";
-            //System.Windows.Controls.ListBoxItem
-            
-            switch (listBox1.SelectedItem.ToString())
-            {
-                case "System.Windows.Controls.ListBoxItem: RUB":
-                    
-                    url = url +"298";
-                    break;
-                case "System.Windows.Controls.ListBoxItem: USD":
-                    url = url + "145";
-                    break;
-                case "System.Windows.Controls.ListBoxItem: EUR":
-                    url = url + "19";
-                    break;
-                case "System.Windows.Controls.ListBoxItem: PLN":
-                    url = url + "293";
-                    break;                
-                default:
-                    break;
-            }
-            switch (listBox2.SelectedItem.ToString())
-            {
-                case "System.Windows.Controls.ListBoxItem: RUB":
+            GetRateById(298);
 
-                    url2 = url2 + "298";
-                    break;
-                case "System.Windows.Controls.ListBoxItem: USD":
-                    url2 = url2 + "145";
-                    break;
-                case "System.Windows.Controls.ListBoxItem: EUR":
-                    url2 = url2 + "19";
-                    break;
-                case "System.Windows.Controls.ListBoxItem: PLN":
-                    url2 = url2 + "293";
-                    break;
-                default:
-                    break;
-            }
+        }
 
-            double otnKRublu = 0;
-            double otnKValute = 0;
-            double vvodDeneg = 0;
-            try
-            {
-                vvodDeneg = Convert.ToDouble(textBox1.Text);
-            }
-            catch (FormatException)
-            {
-                MessageBox.Show("Ошибка ввода количества денег");
-            }
-            
-            
-            
-            //объект, который будет соединяться с сайтом
-            System.Net.WebClient client = new System.Net.WebClient();
-            //объект для десериализации строки (дешифратор)
-            JavaScriptSerializer serializer = new JavaScriptSerializer();
-            
-            String json2 = client.DownloadString(url2);
-            
-            if (listBox1.SelectedItem.ToString() == "System.Windows.Controls.ListBoxItem: BYN")
-            {
-                otnKRublu = 1;
-            }
-            else
-            {
-                //Строка в формате json
-                //{"Cur_ID":298,"Date":"2021-03-18T00:00:00","Cur_Abbreviation":"RUB
-                String json = client.DownloadString(url);
-                //преобразование строки в объект класса Rate
-                Rate izChego = serializer.Deserialize<Rate>(json);
-                otnKRublu = izChego.Cur_Scale;
-            }
-            Rate voCto = serializer.Deserialize<Rate>(json2);
-            otnKValute = Convert.ToDouble( voCto.Cur_OfficialRate);
-            textBox2.Text =(vvodDeneg * otnKRublu * otnKValute).ToString();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        private void CursesUpdate(object sender, EventArgs e)
+        {
 
         }
     }
